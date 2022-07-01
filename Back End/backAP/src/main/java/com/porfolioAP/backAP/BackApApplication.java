dd6dbc542ca25d279128a4f0e7f78b2b64a28194
leonelrocchia <leonelrocchia@gmail.com>
2022-07-01 10:51:28 -0300

    Creacion back + base
    Creamos el back con spring y ya configuramos la conexión con la base de datos. Ahora a codear el back!

---
+++ Back End/backAP/src/main/java/com/porfolioAP/backAP/BackApApplication.java
@@ -0,0 +1,13 @@
+package com.porfolioAP.backAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackApApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackApApplication.class, args);
+	}
+
+}
---
+++ Back End/backAP/src/test/java/com/porfolioAP/backAP/BackApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolioAP.backAP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackApApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
